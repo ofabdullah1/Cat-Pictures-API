694429d7cb4cdb813f19129db1db33d90a136706
John Fulton <john@techelevator.com>
2022-10-27 16:55:56 -0400

    initial commit

---
+++ 15_Review_Day/exercise/CatCards/Controllers/CatController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using CatCards.DAO;
+using CatCards.Models;
+using CatCards.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatCards.Controllers
+{
+
+    public class CatController : ControllerBase
+    {
+        private readonly ICatCardDao cardDao;
+        private readonly ICatFactService catFactService;
+        private readonly ICatPicService catPicService;
+
+        public CatController(ICatCardDao _cardDao, ICatFactService _catFact, ICatPicService _catPic)
+        {
+            catFactService = _catFact;
+            catPicService = _catPic;
+            cardDao = _cardDao;
+        }
+
+
+    }
+}
